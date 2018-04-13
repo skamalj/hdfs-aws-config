e1dcfc585b9a057ca699cabf831cd2467a07bff8
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-13 20:22:44 +0530

    Update setup_ssh.sh

--- setup_ssh.sh
+++ setup_ssh.sh
@@ -1,11 +1,13 @@
 su - hduser
 rm $HOME/.ssh/id_rsa
+rm $HOME/.ssh/known_hosts
 yes "" | ssh-keygen -t rsa -P ""
 cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
 exit 
  
 su - yarn
 rm $HOME/.ssh/id_rsa
+rm $HOME/.ssh/known_hosts
 yes "" | ssh-keygen -t rsa -P ""
 cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys 
 exit 
