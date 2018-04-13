82cbec7398da361f3b213d713ae4f8b72f5f72de
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-13 20:39:23 +0530

    Update set_bashrc.sh

--- set_bashrc.sh
+++ set_bashrc.sh
@@ -1,3 +1,4 @@
+cp ./hdfs-aws-config/hadoopenv /home/ubuntu/
 echo ". /home/ubuntu/hadoopenv" >> /home/ubuntu/.bashrc
 echo ". /home/ubuntu/hadoopenv" >> /home/hduser/.bashrc
 echo ". /home/ubuntu/hadoopenv" >> /home/yarn/.bashrc
