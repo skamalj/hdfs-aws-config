3ffed4a23ef1e8eb63ee5bf0da85045fa7663417
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-13 13:31:02 +0530

    Update setup-storage-and-dir.sh

--- setup-storage-and-dir.sh
+++ setup-storage-and-dir.sh
@@ -1,12 +1,14 @@
+##Mount Storage
 mkdir /data
 mkfs -t ext4 /dev/xvdb
 mount /dev/xvdb /data
 
+##Create Directories
 mkdir -p /data/hadoop/namenode
 mkdir -p /data/hadoop/datanode
 mkdir -p /data/hadoop/logs
 chmod 777 /tmp
-/usr/local
+cd /usr/local
 sudo chown -R hduser:hadoop hadoop
 cd /data
 chown -R hduser:hadoop hadoop
