c71e3e7dcf9bf39700575384ae65787c493de523
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-13 14:49:49 +0530

    Create configure_master.sh

---
+++ configure_master.sh
@@ -0,0 +1,11 @@
+##Update configuration to set master DNS
+cd hdfs-aws-config
+export dns_name=`curl http://169.254.169.254/latest/meta-data/local-hostname`
+sed -i -e 's/dns_to_change/'"$dns_name"'/g' core-site.xml
+sed -i -e 's/dns_to_change/'"$dns_name"'/g' yarn-site.xml
+echo $dns_name > slaves
+
+##Copy files to hadoop config
+cp *.xml /usr/local/hadoop/etc/hadoop/
+cp hadoop-env.sh /usr/local/hadoop/etc/hadoop/
+cp slaves /usr/local/hadoop/etc/hadoop/
