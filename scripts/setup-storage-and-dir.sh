662e7f06b61f6a4e462664ee2d23b69541bd1e66
Kamal <skamalj@gmail.com>
2018-04-23 00:13:16 +0530

    Updated storage

--- scripts/setup-storage-and-dir.sh
+++ scripts/setup-storage-and-dir.sh
@@ -9,7 +9,7 @@ mkdir -p /data/hadoop/datanode
 mkdir -p /data/hadoop/logs
 chmod 777 /tmp
 cd /usr/local
-sudo chown -R hduser:hadoop hadoop
+chown -R hduser:hadoop hadoop
 cd /data
 chown -R hduser:hadoop hadoop
-su -p - hduser -c "chmod g+w /data/hadoop/logs"
+chmod g+w /data/hadoop/logs
