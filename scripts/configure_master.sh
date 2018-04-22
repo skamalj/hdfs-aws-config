8a6c55e894184247d6d6b6ab5915f38a09ceed2f
Kamal <skamalj@gmail.com>
2018-04-22 22:32:24 +0530

    Updated scripts

--- scripts/configure_master.sh
+++ scripts/configure_master.sh
@@ -1,5 +1,5 @@
 ##Update configuration to set master DNS
-cd hdfs-aws-config
+cd hdfs-aws-config/config
 dns_name=`curl http://169.254.169.254/latest/meta-data/local-hostname`
 sed -i -e 's/dns_to_change/'"$dns_name"'/g' core-site.xml
 sed -i -e 's/dns_to_change/'"$dns_name"'/g' yarn-site.xml
@@ -12,4 +12,4 @@ cp mapred-env.sh /usr/local/hadoop/etc/hadoop/
 cp yarn-env.sh /usr/local/hadoop/etc/hadoop/
 
 cp slaves /usr/local/hadoop/etc/hadoop/
-cd ..
+cd ../..
--- scripts/set_bashrc.sh
+++ scripts/set_bashrc.sh
@@ -1,4 +1,4 @@
-cp ./hdfs-aws-config/hadoopenv /home/ubuntu/
+cp ./hdfs-aws-config/config/hadoopenv /home/ubuntu/
 echo ". /home/ubuntu/hadoopenv" >> /home/ubuntu/.bashrc
 echo ". /home/ubuntu/hadoopenv" >> /home/hduser/.bashrc
 echo ". /home/ubuntu/hadoopenv" >> /home/yarn/.bashrc
