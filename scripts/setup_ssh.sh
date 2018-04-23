34b459e663d07fe1585af3e11cace17d858ff4b1
Kamal <skamalj@gmail.com>
2018-04-23 22:57:00 +0530

    Updated SSH

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,9 +1,12 @@
 su - hduser << EOF
 yes "" | ssh-keygen -t rsa -P ""
 cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
+ssh-keyscan -H localhost >> /home/hduser/.ssh/known_hosts
+ssh-keyscan -H 0.0.0.0 >> /home/hduser/.ssh/known_hosts
 EOF
 
 su - yarn << EOF
 yes "" | ssh-keygen -t rsa -P ""
 cat /home/yarn/.ssh/id_rsa.pub >> /home/yarn/.ssh/authorized_keys
+ssh-keyscan -H localhost >> /home/hduser/.ssh/known_hosts
 EOF
