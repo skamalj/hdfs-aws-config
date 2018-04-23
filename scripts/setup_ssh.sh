ee4493c2fe8a0e322b4a2e7540f32c6b0765cc1a
Kamal <skamalj@gmail.com>
2018-04-23 23:24:02 +0530

    Created hdfs startup file

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,12 +1,14 @@
 su - hduser << EOF
 yes "" | ssh-keygen -t rsa -P ""
 cat /home/hduser/.ssh/id_rsa.pub >> /home/hduser/.ssh/authorized_keys
-ssh-keyscan -H localhost >> /home/hduser/.ssh/known_hosts
+dns_name=`curl http://169.254.169.254/latest/meta-data/local-hostname`
+ssh-keyscan -H $dns_name >> /home/hduser/.ssh/known_hosts
 ssh-keyscan -H 0.0.0.0 >> /home/hduser/.ssh/known_hosts
 EOF
 
 su - yarn << EOF
 yes "" | ssh-keygen -t rsa -P ""
 cat /home/yarn/.ssh/id_rsa.pub >> /home/yarn/.ssh/authorized_keys
-ssh-keyscan -H localhost >> /home/yarn/.ssh/known_hosts
+dns_name=`curl http://169.254.169.254/latest/meta-data/local-hostname`
+ssh-keyscan -H $dns_name >> /home/yarn/.ssh/known_hosts
 EOF
