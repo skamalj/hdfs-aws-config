83601f5cb2906824071a6ead1829638c11a4fae5
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-24 13:42:18 +0530

    Update upload_master_config_to_s3.sh

--- scripts/upload_master_config_to_s3.sh
+++ scripts/upload_master_config_to_s3.sh
@@ -1,5 +1,16 @@
-cd /home/ubuntu/hdfs-aws-config/scripts
-aws s3 cp *.xml s3::/$1
-aws s3 cp hadoop-env.sh s3::/$1
-aws s3 cp mapred-env.sh s3::/$1
-aws s3 cp yarn-env.sh s3::/$1
+cd /home/ubuntu/hdfs-aws-config/config
+cp  /home/hduser/.ssh/id_rsa.pub ./id_rsa_hduser.pub
+cp  /home/yarn/.ssh/id_rsa.pub ./id_rsa_yarn.pub
+su - ubuntu << EOF
+cd /home/ubuntu/hdfs-aws-config/config
+aws s3 cp mapred-site.xml s3://$1
+aws s3 cp hdfs-site.xml s3://$1
+aws s3 cp yarn-site.xml s3://$1
+aws s3 cp core-site.xml s3://$1
+aws s3 cp hadoopenv s3://$1
+aws s3 cp hadoop-env.sh s3://$1
+aws s3 cp mapred-env.sh s3://$1
+aws s3 cp yarn-env.sh s3://$1
+aws s3 cp  id_rsa_hduser.pub s3://$1/id_rsa_hduser.pub
+aws s3 cp  id_rsa_yarn.pub s3://$1/id_rsa_yarn.pub
+EOF
