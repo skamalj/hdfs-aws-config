4d0a9c3ee37297625ff58a537ce0da8ad6c7729c
skamalj <32254183+skamalj@users.noreply.github.com>
2018-04-24 17:09:01 +0530

    Update start_slavenode.sh

--- scripts/start_slavenode.sh
+++ scripts/start_slavenode.sh
@@ -1,7 +1,7 @@
 . /home/ubuntu/hadoopenv
 su - hduser << EOF 
-$HADOOP_INSTALL/bin/hadoop-daemon.sh start datanode
+$HADOOP_INSTALL/sbin/hadoop-daemon.sh start datanode
 EOF
 su - yarn << EOF
-$HADOOP_INSTALL/bin/yarn-daemon.sh start nodemanager
+$HADOOP_INSTALL/sbin/yarn-daemon.sh start nodemanager
 EOF
