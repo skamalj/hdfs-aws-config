30638637763be8fac60232739331075cdc9c1be4
Kamal <skamalj@gmail.com>
2018-04-26 23:59:54 +0530

    Create Image script

--- scripts/set_bashrc.sh
+++ scripts/set_bashrc.sh
@@ -1,3 +1,4 @@
+##This scripts copies file will all hadoop env variables to .bashrc files of required users 
 cp ./hdfs-aws-config/config/hadoopenv /home/ubuntu/
 echo ". /home/ubuntu/hadoopenv" >> /home/ubuntu/.bashrc
 echo ". /home/ubuntu/hadoopenv" >> /home/hduser/.bashrc
